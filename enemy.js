cde7948102c2d40d1741e124264cf9c91b12d7ba
JoshuaBrophy <josh666brophy@hotmail.co.uk>
2024-02-16 11:13:12 +0000

    added and subtracted
    ive overhauld the enemy and player class
    
    ive added alot of the narration for the game and tryed to start making the combat system work the way i want it to also i added my multi choice into the game too  by using a switch function im looking into usin the multi choice in my combat too
    
    now im looking into my battle mechanics  atm

--- enemy.js
+++ enemy.js
@@ -1,28 +1,4 @@
-class Player {
-    constructor(name, difficulty) {
-      this.name = name;
-      this.difficulty = difficulty;
-      this.score = 0;
-      this.health = 100;
-      this.attackDamage = this.generateRandomDamage(3, 5);
-      this.speed = 10;
-      this.luck = this.generateRandomLuck();
-    }
-
-    increaseScore(points) {
-      this.score += points;
-    }
-
-    generateRandomDamage(min, max) {
-      return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
-    generateRandomLuck() {
-      return Math.min(Math.floor(Math.random() * 21), 20);
-    }
-  }
-    
-  class Enemy {
+export default class Enemy {
     constructor(name, difficulty, health, minAttackDamage, maxAttackDamage, speed, luck) {
       this.name = name;
       this.difficulty = difficulty;
@@ -34,6 +10,7 @@ class Player {
     }
   
     attack(player) {
+      
       const hitChance = Math.random() * 100;
       if (hitChance < player.luck) {
         console.log(`${this.name} missed the attack!`);
@@ -43,6 +20,8 @@ class Player {
         console.log(`${this.name} attacked ${player.name} for ${damage} damage!`);
         console.log(`${player.name}'s health: ${player.health}`);
       }
+  
+     
     }
   
     generateRandomDamage(min, max) {
---
+++ player.js
@@ -0,0 +1,25 @@
+class Player {
+    constructor(name, difficulty) {
+      this.name = name;
+      this.difficulty = difficulty;
+      this.score = 0;
+      this.health = 100;
+      this.attackDamage = this.generateRandomDamage(3, 5);
+      this.speed = 10;
+      this.luck = this.generateRandomLuck();
+    }
+
+    increaseScore(points) {
+      this.score += points;
+    }
+
+    generateRandomDamage(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    generateRandomLuck() {
+      return Math.min(Math.floor(Math.random() * 21), 20);
+    }
+  }
+    
+  
